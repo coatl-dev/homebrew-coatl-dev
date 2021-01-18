0f9c614bfa611bc583afe6c821afaf084955e675
César Román <thecesrom@gmail.com>
2021-01-18 14:33:06 -0800

    Added ignition formulae Added formulae for latest stable releases currently under active development versions: 7.9.17, 8.0.17, and 8.1.1

---
+++ Formula/ignition@8.0.17.rb
@@ -0,0 +1,33 @@
+class IgnitionAT8017 < Formula
+  desc "Unlimited Platform for SCADA and so much more"
+  homepage "https://inductiveatumation.com/"
+  url "https://files.inductiveautomation.com/release/ia/8.0.17/20201112-1050/Ignition-osx-8.0.17.zip", using: :curl, referer: "https://inductiveautomation.com/"
+  sha256 "e873f8cc0a7f1f7d92379a212f71c15646712bede748c6444b47fe2a21e31447"
+  license "https://inductiveautomation.com/ignition/license"
+
+  def install
+    libexec.install Dir["*"]
+    # Make files executable and add symlinks
+    %w[gwcmd.sh ignition.sh ignition-gateway].each do |cmd|
+      chmod "u=wrx,go=rx", "#{libexec}/#{cmd}"
+    end
+    # Create symlink for ignition.sh
+    bin.install_symlink "#{libexec}/ignition.sh" => "ignition-#{version}"
+    # Update com.inductiveautomation.ignition.plist
+    inreplace "#{libexec}/com.inductiveautomation.ignition.plist" do |s|
+      s.gsub! "/usr/local/bin/ignition", "/usr/local/bin/ignition-#{version}"
+    end
+    # Link plist
+    prefix.install_symlink "#{libexec}/com.inductiveautomation.ignition.plist" => "#{plist_name}.plist"
+  end
+
+  def post_install
+    %w[License.html Notice.txt README.txt].each do |f|
+      libexec.install "#{prefix}/#{f}"
+    end
+  end
+
+  test do
+    assert_match "Ignition-Gateway", shell_output("#{bin}/ignition-#{version} status")
+  end
+end
---
+++ Formula/ignition@8.1.1.rb
@@ -0,0 +1,33 @@
+class IgnitionAT811 < Formula
+  desc "Unlimited Platform for SCADA and so much more"
+  homepage "https://inductiveatumation.com/"
+  url "https://files.inductiveautomation.com/release/ia/8.1.1/20201208-0843/Ignition-osx-8.1.1.zip", using: :curl, referer: "https://inductiveautomation.com/"
+  sha256 "a332a9ff9705bba3e2254976378be4835de7ed85a3ff448d70111585b61419ea"
+  license "https://inductiveautomation.com/ignition/license"
+
+  def install
+    libexec.install Dir["*"]
+    # Make files executable
+    %w[gwcmd.sh ignition.sh ignition-gateway].each do |cmd|
+      chmod "u=wrx,go=rx", "#{libexec}/#{cmd}"
+    end
+    # Create symlink for ignition.sh
+    bin.install_symlink "#{libexec}/ignition.sh" => "ignition-#{version}"
+    # Update com.inductiveautomation.ignition.plist
+    inreplace "#{libexec}/com.inductiveautomation.ignition.plist" do |s|
+      s.gsub! "/usr/local/bin/ignition", "/usr/local/bin/ignition-#{version}"
+    end
+    # Link plist
+    prefix.install_symlink "#{libexec}/com.inductiveautomation.ignition.plist" => "#{plist_name}.plist"
+  end
+
+  def post_install
+    %w[License.html Notice.txt README.txt].each do |f|
+      libexec.install "#{prefix}/#{f}"
+    end
+  end
+
+  test do
+    assert_match "Ignition-Gateway", shell_output("#{bin}/ignition-#{version} status")
+  end
+end
