8a954dcab7fe03919a98a79658a6e882c19239bf
César Román <thecesrom@gmail.com>
2021-05-25 18:55:15 -0700

    feat(ignition): add Ignition 8.1.6 formulae

---
+++ Formula/ignition@8.1.6.rb
@@ -0,0 +1,37 @@
+class IgnitionAT816 < Formula
+  desc "Unlimited Platform for SCADA and so much more"
+  homepage "https://inductiveautomation.com/"
+  url "https://files.inductiveautomation.com/release/ia/8.1.6/20210525-1228/Ignition-osx-8.1.6.zip",
+      referer: "https://inductiveautomation.com/"
+  sha256 "563439192b69751fcc5d2a31c9b0c7c8eae118bc00ad7065d871651b7ca50868"
+  license :cannot_represent
+
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["*"]
+    # Make files executable
+    %w[gwcmd.sh ignition.sh ignition-gateway].each do |cmd|
+      chmod "u=wrx,go=rx", "#{libexec}/#{cmd}"
+    end
+    # Create symlink for ignition.sh
+    bin.install_symlink "#{libexec}/ignition.sh" => "ignition-#{version}"
+    # Update com.inductiveautomation.ignition.plist
+    inreplace "#{libexec}/com.inductiveautomation.ignition.plist" do |s|
+      s.gsub! "/usr/local/bin/ignition", "/usr/local/bin/ignition-#{version}"
+    end
+    # Link plist
+    prefix.install_symlink "#{libexec}/com.inductiveautomation.ignition.plist" => "#{plist_name}.plist"
+  end
+
+  def post_install
+    %w[License.html Notice.txt README.txt].each do |f|
+      libexec.install "#{prefix}/#{f}"
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/ignition-#{version} 2>&1", 1)
+    assert_match "#{bin}/ignition-#{version}", output
+  end
+end
