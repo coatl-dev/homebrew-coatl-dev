3321791f7453826da9305ad526f3e124b87b493f
César Román <cesar@coatl.dev>
2024-04-02 00:10:59 -0700

    jython@2.7.3: depend on openjdk@17 (#90)

--- Formula/jython@2.7.3.rb
+++ Formula/jython@2.7.3.rb
@@ -5,7 +5,7 @@ class JythonAT273 < Formula
   sha256 "3ffc25c5257d2028b176912a4091fe048c45c7d98218e52d7ce3160a62fdc9fc"
   license "PSF-2.0"
 
-  depends_on "openjdk@11"
+  depends_on "openjdk@17"
 
   def install
     system "java", "-jar", cached_download, "-s", "-t", "standard", "-d", libexec
