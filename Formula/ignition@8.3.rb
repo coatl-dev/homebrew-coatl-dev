8d360aa953aef93d7d3b413e97e1c1fd974fd3ac
César Román <cesar@coatl.dev>
2025-07-30 17:27:40 -0700

    ignition: add 8.3.0-beta1 (#104)

---
+++ Formula/ignition@8.3.rb
@@ -0,0 +1,96 @@
+class IgnitionAT83 < Formula
+  desc "Unlimited Platform for SCADA and so much more"
+  homepage "https://inductiveautomation.com/"
+  if OS.mac?
+    os = "macOs"
+    arch = "aarch64"
+    sha = "c7554662513dca7994d6a624a271140c58f830f56b8d33270e367a3cef372237"
+  else
+    os = "linux"
+    arch = "x86"
+    sha = "6a6bf34dced3a6a62434ff1090190f7319b2d0d401b1cd31f1f2e58012479548"
+  end
+  url "https://files.inductiveautomation.com/release/ia/8.3.0-beta1/20250725-1612/Ignition-#{os}-#{arch}-64-8.3.0-beta1.zip",
+      referer: "https://inductiveautomation.com/"
+  version "8.3.0-beta1"
+  sha256 sha.to_s
+  license :cannot_represent
+
+  livecheck do
+    url "https://inductiveautomation.com/downloads/ignition/"
+    strategy :page_match
+    regex(/"version"\s*:\s*"(8.3.(?:\d+-(?:beta|rc)\d+\b)?)"/i)
+  end
+
+  def install
+    # Relocate data
+    (etc/"ignition/8.3").mkpath unless (etc/"ignition/8.3/data").exist?
+    etc.install "data" => "ignition/8.3/data" unless (etc/"ignition/8.3/data").exist?
+    rm_r "data"
+
+    # Relocate logs
+    (var/"ignition/8.3/logs").mkpath unless (var/"ignition/8.3/logs").exist?
+    var.install "logs" => "ignition/8.3/logs" unless (var/"ignition/8.3/logs").exist?
+    rm_r "logs"
+
+    # Install
+    libexec.install Dir["*"]
+
+    # Make files executable
+    %w[gwcmd.sh ignition.sh ignition-secrets-tool.sh ignition-util.sh ignition-gateway].each do |cmd|
+      chmod "u=wrx,go=rx", "#{libexec}/#{cmd}"
+    end
+
+    # Create symlinks
+    bin.install_symlink "#{libexec}/ignition.sh" => "ignition"
+    libexec.install_symlink "#{etc}/ignition/8.3/data" => "data"
+    libexec.install_symlink "#{var}/ignition/8.3/logs" => "logs"
+  end
+
+  def post_install
+    # Relocate files
+    %w[License.html Notice.txt README.txt].each do |f|
+      libexec.install "#{prefix}/#{f}" if File.exist?("#{prefix}/#{f}")
+    end
+
+    # Unzip the new runtime
+    system bin/"ignition", "checkruntimes"
+
+    # Update ignition.conf
+    system bin/"ignition", "runupgrader"
+  end
+
+  def caveats
+    s = <<~EOS
+      The data and logs folders have been symlinked to:
+        data: #{etc}/ignition/8.3/data
+        logs: #{var}/ignition/8.3/logs
+    EOS
+    s += find_other_installations
+    s
+  end
+
+  def find_other_installations
+    n = 0
+    s = ""
+    # Check for the typical location
+    n +=1 if Dir.exist?("/usr/local/ignition")
+    # Check for other Homebrew installations
+    Dir["#{HOMEBREW_PREFIX}/Cellar/ignition**"].each do
+      n += 1
+    end
+    if n > 1
+      s = <<~EOS
+
+        Other installations have been found which may interfere with a Homebrew-built
+        Ignition Gateway from starting up correctly.
+      EOS
+    end
+    s
+  end
+
+  test do
+    output = shell_output("#{bin}/ignition")
+    assert_match "#{libexec}/ignition.sh", output.lines.first
+  end
+end
